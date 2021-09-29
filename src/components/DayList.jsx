a7bdde1ba040b33d704a7894d4797415b0fef746
Rdarke <rickydarke1@gmail.com>
2021-09-28 21:34:07 -0700

    Update DayList.jsx to dynamicly render day information

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,8 +1,20 @@
 import React from "react";
+import DayListItem from "components/DayListItem";
+
+//..
+// Dynamicly renders multiple days.
+//..
 
 export default function DayList(props) {
+  const list = props.days.map((day) => (
+    <DayListItem
+      key={day.id}
+      name={day.name}
+      spots={day.spots}
+      selected={day.name === props.day}
+      setDay={props.setDay}
+    />
+  ));
 
-  return (
-    <ul></ul>
-  );
+  return <ul>{list}</ul>;
 }
--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,7 @@ import "index.scss";
 
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
 
 //..
 // Start of the main Button stories.
@@ -29,9 +30,9 @@ storiesOf("Button", module)
   ));
 
   //..
-  //Start of the DayListItem Stories
+  // Start of the DayListItem Stories
   //..
-  
+
   storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -41,4 +42,37 @@ storiesOf("Button", module)
   .add("Full", () => <DayListItem name="Monday" spots={0} />)
   .add("Clickable", () => (
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
+  ));
+
+  //...
+  //Start of DayList stories...
+  //...
+  // Includes mock data below. For testing only.
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
+
+  storiesOf("DayList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Monday", () => (
+    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+  ))
+  .add("Tuesday", () => (
+    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
   ));
