8afb50ecbac030c53e21227b4e3ed1db29e744bc
Rdarke <rickydarke1@gmail.com>
2021-09-29 16:27:01 -0700

    add map logic to InterviewerList component

--- src/components/InterviewerList.jsx
+++ src/components/InterviewerList.jsx
@@ -1,18 +1,27 @@
 import React from "react";
-import "components/InterviewerListItem.scss";
-import classNames from "classnames";
+import "components/InterviewerList.scss";
+import InterviewerListItem from "components/InterviewerListItem";
 
 //..
 // InterviewerList Component, will render a full list of interviwer icons.
 //..
 
 export default function InterviewerList(props) {
-  const { interviewers, interviewer, setInterviewer } = props;
+
+  const interviews = props.interviewers.map((int) => (
+    <InterviewerListItem
+      key={int.id}
+      name={int.name}
+      avatar={int.avatar}
+      selected={int.id === props.interviewer}
+      setInterviewer={int.interviewer}
+    />
+  ));
 
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
-      <ul className="interviewers__list"></ul>
+      <ul className="interviewers__list">{interviews}</ul>
     </section>
   );
-}
+};
