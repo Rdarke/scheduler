dff91a8c5c76c357eb954c597fb54747789b2963
Rdarke <rickydarke1@gmail.com>
2021-09-28 20:42:42 -0700

    Add function to format spots via DayListitems.jsx

--- src/components/DayListItem.jsx
+++ src/components/DayListItem.jsx
@@ -14,10 +14,23 @@ export default function DayListItem(props) {
     "day-list__item--full": spots === 0,
   });
 
+  // Function formats spots to render user messaging based on available spots.
+  const formatSpots = () => {
+    if (spots === 0) {
+      return "no spots remaining"
+    }
+    if (spots === 1) {
+      return `${spots} spot remaining`
+    }
+    if (spots >= 2) {
+      return `${spots} spots remaining`
+    }
+  };
+
   return (
     <li className={dayClass} onClick={() => setDay(name)}>
       <h2>{name}</h2>
-      <h3>{spots}</h3>
+      <h3>{formatSpots()}</h3>
     </li>
   );
-}
+};
