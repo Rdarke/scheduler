228caa6e58ec9037a345607cfaf19e5b440f9f79
Rdarke <rickydarke1@gmail.com>
2021-09-29 18:25:20 -0700

    Add Confirm.jsx component stories

--- src/components/Appointment/Confirm.jsx
+++ src/components/Appointment/Confirm.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import Button  from "components/Button";
 
-export default function Confirm() {
+export default function Confirm(props) {
+  const {message, onConfirm, onCancel} = props
+
   return (
     <main className="appointment__card appointment__card--confirm">
       <h1 className="text--semi-bold">Delete the appointment?</h1>
--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
 
 //..
 // Start of the main Button stories.
@@ -168,6 +169,11 @@ storiesOf("Button", module)
         interviewer={interviewer} 
         onEdit={action("onEdit")} 
         onDelete={action("onDelete")} 
+        />)
+        .add("Confirm", () => <Confirm 
+        message="Delete the appointment?" 
+        onConfirm={action("onConfirm")} 
+        onCancel={action("onCancel")} 
         />)
 
 
