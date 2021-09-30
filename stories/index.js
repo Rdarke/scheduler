8f8963c116c3bd23f62792a79e7e776175a88fd4
Rdarke <rickydarke1@gmail.com>
2021-09-29 17:33:52 -0700

    Add new stories for Appointment components

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,7 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index";
 
 //..
 // Start of the main Button stories.
@@ -113,7 +114,7 @@ storiesOf("Button", module)
         id={interviewer.id}
         name={interviewer.name}
         avatar={interviewer.avatar}
-        setInterviewer={action("setInterviewer")}
+        setInterviewer={event => action("setInterviewer")(interviewer.id)}
       />
     ));
 
@@ -146,3 +147,16 @@ storiesOf("Button", module)
           setInterviewer={action("setInterviewer")}
         />
       ));
+
+      //...
+      // Appointment component testing story....
+      //..
+
+    storiesOf("Appointment", module)
+      .addParameters({
+        backgrounds: [{ name: "white", value: "#fff", default: true }]
+      })
+      .add("Appointment", () => <Appointment />)
+      .add("Appointment with Time", () => <Appointment time="12pm" />);
+      
+      
