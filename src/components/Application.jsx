1404674365b0d0b8387aa5f3a03b11fbd65cbbf5
Rdarke <rickydarke1@gmail.com>
2021-09-30 14:46:28 -0700

    Add test data to Application.jsx

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -22,6 +22,67 @@ const days = [
   },
 ];
 
+// testing data for appointments
+const appointments = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+  id: 3,
+    time: "3pm",
+    interview: {
+      student: "Ricky Bobby",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 3,
+      time: "5pm",
+      interview: {
+        student: "Fanky Lillian",
+        interviewer: {
+          id: 3,
+          name: "Mildred Nazir",
+          avatar: "https://i.imgur.com/T2WwVfS.png",
+        }
+      }
+    },
+    {
+      id: 2,
+      time: "12pm",
+    },
+    {
+      id: 2,
+      time: "5pm",
+      interview: {
+        student: "Hoolio Jones",
+        interviewer: {
+          id: 5,
+          name: "Sven Jones",
+          avatar: "https://i.imgur.com/twYrpay.jpg",
+        }
+      }
+    },
+];
+
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday")
 
