6acfe8a174e09aa742abd6213ccac99855b984cb
Rdarke <rickydarke1@gmail.com>
2021-09-29 18:43:28 -0700

    Create Error.jsx & import to stories

--- stories/index.js
+++ stories/index.js
@@ -16,6 +16,7 @@ import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
 
 //..
 // Start of the main Button stories.
