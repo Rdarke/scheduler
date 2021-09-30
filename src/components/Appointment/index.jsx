90f627bebd7110b275e91bdd600d1505e78973f8
Rdarke <rickydarke1@gmail.com>
2021-09-29 18:02:17 -0700

    Create Show.jsx components file & add to stories

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "components/Appointment/styles.scss";
 
-export default function Appointment(props) {
+export default function Appointment() {
   return (
     <article className="appointment"></article>
   );
--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,8 @@ import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index";
 import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 //..
 // Start of the main Button stories.
@@ -160,5 +162,6 @@ storiesOf("Button", module)
       .add("Appointment", () => <Appointment />)
       .add("Appointment with Time", () => <Appointment time="12pm" />)
       .add("Header", () => <Header time="12pm" />)
+      .add("Empty", () => <Empty onAdd={action("onAdd")} />)
 
 
