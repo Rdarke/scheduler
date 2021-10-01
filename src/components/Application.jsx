c6bd6421227c642fd8c66ff5dab5679ffd87eb20
Rdarke <rickydarke1@gmail.com>
2021-09-30 18:16:10 -0700

    Install axios and import into Application.jsx

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import DayList from "components/DayList";
 import Appointment from "components/Appointment/index"
 
