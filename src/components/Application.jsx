f526cb206413295ed10c68d79d64c39354c317e6
Rdarke <rickydarke1@gmail.com>
2021-10-01 16:39:12 -0700

    Add getInterview function to selectors.js & unit test

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -14,7 +14,8 @@ export default function Application() {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments:{}
+    appointments:{},
+    interviewers:{}
   })
   const setDay = day => setState({ ...state, day });
 
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,16 +3,30 @@
 export function getAppointmentsForDay(state, day) {
   const results = [];
   // finds the array via day argument.
-  const locateDay = state.days.filter( dayObj => dayObj.name === day);
-  
+  const locateDay = state.days.filter((dayObj) => dayObj.name === day);
   // returns empty array if locateDay is empty
   if (locateDay.length === 0) {
     return results;
   }
-
   // push the desired object values into a new array based on the above keys.
   for (const val of locateDay[0].appointments) {
     results.push(state.appointments[val]);
   }
   return results;
 }
+
+// function provides a new object containing the interview data when passed an object that contains the interviewer
+export function getInterview(state, interview) {
+  if (interview === null) {
+    return null;
+  }
+  let results = {}
+  const id = interview.interviewer;
+  const interviewer = state.interviewers[id]
+
+  results = {
+    "student": interview.student,
+    "interviewer": interviewer,
+  };
+  return results;
+};
