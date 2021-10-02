5bb05aacc0364d7afd6e5847ace43c93b46dc149
Rdarke <rickydarke1@gmail.com>
2021-10-02 11:53:02 -0700

    Update index.jsx appointments to use.. useVisualMode custom hook logic

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -3,17 +3,30 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import useVisualMode from "hooks/useVisualMode"
 
+// supporting mode constants for useVisualMode custom hook.
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+
+// Appointments gains props from Application.jsx
 export default function Appointment(props) {
   const {interview, time} = props
-  console.log("Show.jsx: interview", interview);
+
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={time}/>
-      {interview ?
-      <Show student={interview.student} interviewer={interview.interviewer}/> :
-      <Empty />}
+      {mode === EMPTY && <Empty onAdd={() => console.log("Clicked onAdd")} />}
+      {mode === SHOW && (
+        <Show 
+        student={interview.student} 
+        interviewer={interview.interviewer}
+        />
+      )}
     </article>
   );
 };
