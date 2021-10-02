7880065ac02e5e60d621b9cd104512510188222a
Rdarke <rickydarke1@gmail.com>
2021-10-01 23:07:44 -0700

    Add a transitional state function via UseVisualMode

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -1,8 +1,16 @@
 import React, { useState } from 'react'
 
 export default function useVisualMode(initial) {
-
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return {mode};
+  const transition = function(newMode) {
+    setMode(newMode);
+  };
+
+  const back = function() {
+
+  }
+
+  return {mode, transition, back};
 };
