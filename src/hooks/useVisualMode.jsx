51eff04aa40542b1642ebefbd0453de9691c2667
Rdarke <rickydarke1@gmail.com>
2021-10-02 11:33:03 -0700

    Add a replace argument to transition function within UseVisualMode.jsx

--- src/hooks/useVisualMode.jsx
+++ src/hooks/useVisualMode.jsx
@@ -4,17 +4,25 @@ export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
   const [history, setHistory] = useState([initial]);
 
-  const transition = function(newMode) {
+  // allows the transition of the mode value to the new mode value.
+  const transition = function(newMode, replace = false) {
+    if (replace === true) {
+      setHistory([...history]);
+    } else {
+      setHistory([...history, newMode]);
+    }
     setMode(newMode);
-    setHistory([...history, newMode]);
   };
 
+  // back represents a push & pop of the stack via the history array.
   const back = function() {
     if (history.length > 1) {
+
       setHistory(history => {
         const newHistory = [...history].slice(0, history.length - 1);
         setMode(newHistory[newHistory.length - 1]);
         return newHistory
+
       });
     }
   }
